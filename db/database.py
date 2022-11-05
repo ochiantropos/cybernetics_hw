3cc74df3a409eb0e4c4d82384de6bbfbb0f1a784
Vadim Dembitskii <vadim_dembitskii@epam.com>
2022-11-05 21:15:10 +0200

    made basic classes for work with database

---
+++ db/database.py
@@ -0,0 +1,105 @@
+from abc import ABC, abstractmethod
+import shelve
+from sequence import AbstractSequence
+import os
+
+
+class AbstractDatabase(ABC):
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    @abstractmethod
+    def write(self, obj):
+        pass
+
+    @abstractmethod
+    def write_all_objects(self, obj_list):
+        pass
+
+    @abstractmethod
+    def get_obj_by_id(self, pk):
+        pass
+
+    @abstractmethod
+    def get_all_objects(self):
+        pass
+
+    @abstractmethod
+    def delete_obj_by_id(self, pk):
+        pass
+
+
+class ShelveDatabase(AbstractDatabase):
+    data_folder = os.path.join(os.path.dirname(__file__), "../data/")
+
+    def __init__(self, db_name: str, sequence_strategy: AbstractSequence.__class__):
+        super().__init__(db_name)
+        self.sequence_strategy = sequence_strategy(self.get_last_id())
+
+    def write(self, obj):
+        with self._open_db() as db:
+            self._write(obj, db)
+
+    def write_all_objects(self, obj_list):
+        with self._open_db() as db:
+            for obj in obj_list:
+                self._write(obj, db)
+
+    # TODO: throw exception instead of returning None
+    def get_obj_by_id(self, pk):
+        with self._open_db() as db:
+            if str(pk) in db:
+                return self._get_obj(pk, db)
+            else:
+                return None
+
+    def get_all_objects(self):
+        with self._open_db() as db:
+            keys = set(db.keys())
+            objects = []
+            for k in keys:
+                objects.append(self._get_obj(k, db))
+            return objects
+
+    # TODO: throw exception instead of passing
+    def delete_obj_by_id(self, pk):
+        with self._open_db() as db:
+            if str(pk) in db:
+                self._delete_obj(pk, db)
+            else:
+                pass
+
+    def _delete_obj(self, pk, db):
+        del db[str(pk)]
+
+    def _write(self, obj, db):
+        if self._is_new(obj):
+            pk = self.sequence_strategy.next()
+            self._set_id(obj, id)
+            db[str(pk)] = obj
+        else:
+            db[str(obj.pk)] = obj
+
+    def _get_obj(self, pk, db):
+        obj = db[str(pk)]
+        self._set_id(obj, pk)
+        return obj
+
+    @staticmethod
+    def _is_new(obj):
+        return not hasattr(obj, 'pk')
+
+    @staticmethod
+    def _set_id(obj, pk):
+        setattr(obj, 'pk', pk)
+
+    def _open_db(self):
+        return shelve.open(self.data_folder + self.db_name, "c")
+
+    def get_last_id(self):
+        with self._open_db() as db:
+            keys = set(map(int, db.keys()))
+            if len(keys) == 0:
+                return 1
+            return max(keys)
---
+++ db/sequence.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractSequence(ABC):
+
+    def __init__(self, last_id):
+        self.last_id = last_id
+
+    @abstractmethod
+    def next(self):
+        pass
+
+    @abstractmethod
+    def see_next(self):
+        pass
+
+    def get_current(self):
+        return self.last_id
+
+
+class IncrementalSequenceStrategy(AbstractSequence):
+
+    def next(self):
+        self.last_id += 1
+        return self.last_id
+
+    def see_next(self):
+        return self.last_id + 1
