77f6e1aeb301734877ee48389b12a38da5ad9a53
Vadim Dembitskii <vadim_dembitskii@epam.com>
2022-11-06 12:35:18 +0200

    made unitests for remaining uncovered in ShelvePersistence

--- tests/db/ShelveDatabaseTest.py
+++ tests/db/ShelveDatabaseTest.py
@@ -36,11 +36,21 @@ class ShelvePersistenceTest(unittest.TestCase):
             if file_name.startswith("players_test"):
                 os.remove(os.path.join(my_dir, file_name))
 
-    def test_save(self):
+    def test_saves_object(self):
         player = Player("John", "Doe")
         player = self.repository.save(player)
         self.assertTrue(hasattr(player, 'pk'))
         self.assertEqual(player.pk, self.sequence.get_current())
+
+    def test_updates_existing_object(self):
+        player = Player("John", "Doe")
+        player = self.repository.save(player)
+        self.assertEqual(player.name, "John")
+        player.name = "John1"
+        self.repository.save(player)
+        updated_player = self.repository.sync(player)
+        self.assertEqual(updated_player.name, "John1")
+        self.assertEqual(updated_player.pk, player.pk)
 
     def test_save_all(self):
         players = [Player("John1", "Doe1"), Player("John2", "Doe2"), Player("John3", "Doe3"), Player("John4", "Doe4")]
@@ -57,6 +67,9 @@ class ShelvePersistenceTest(unittest.TestCase):
         self.assertEqual(player.name, retrieved_player.name)
         self.assertEqual(player.surname, retrieved_player.surname)
 
+    def test_get_by_id_throws_exception_given_nonexistent_id(self):
+        self.assertRaises(PersistentObjectDoesNotExists, self.repository.get_by_id, 100500)
+
     def test_get_all(self):
         self.remove_test_db_files()
         self.repository.refresh_persistence()
@@ -72,6 +85,9 @@ class ShelvePersistenceTest(unittest.TestCase):
         self.repository.delete_by_id(player.pk)
         self.assertRaises(PersistentObjectDoesNotExists, self.repository.get_by_id, player.pk)
 
+    def test_delete_by_id_throws_exception_given_nonexistent_id(self):
+        self.assertRaises(PersistentObjectDoesNotExists, self.repository.delete_by_id, 100500)
+
     def test_sync(self):
         player = Player("Sync", "Sync")
         self.repository.save(player)
@@ -81,5 +97,11 @@ class ShelvePersistenceTest(unittest.TestCase):
         self.assertEqual(player.name, synced_player.name)
         self.assertEqual(player.surname, synced_player.surname)
 
+    def test_sync_throws_exception_given_obj_with_nonexistent_id(self):
+        player = Player("Sync", "Sync", 100500)
+        self.assertRaises(PersistentObjectDoesNotExists, self.repository.sync, player)
 
-
+    def test_sync_returns_none_given_non_persistent_obj(self):
+        player = Player("Sync", "Sync")
+        synced_player = self.repository.sync(player)
+        self.assertIsNone(synced_player)
