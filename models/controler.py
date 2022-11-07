4651b0fa6849f0a38bbfe6b6a84956534a17f0f6
óchiAnthropos <58050924+ochiantropos@users.noreply.github.com>
2022-11-07 22:50:35 +0200

    Revert "Add 2 controlers and they system"
    This reverts commit 5a056cf0fa2fe5ecbfd3473b432d85091bb0ca94.

--- models/controler.py
+++ models/controler.py
@@ -1,16 +1,92 @@
-from controlers.AbstractControler import AbstractControler
+from abc import ABC, abstractmethod
+from pprint import pprint
+
+class AbstractControler( ABC ):
+	def __init__( self, controler_type : str ):
+		self.controler_type = controler_type
+
+
+	@abstractmethod
+	def run_session( self ):
+		pass
+
+
+
+class VievCommandsControler( AbstractControler ):
+	def __init__( self, controler_type : str, elements : dict ):
+		super().__init__(controler_type)
+		print(elements)
+
+
+	def run_session( self ):
+		print("ahah sosi")
+
+
+
+class LoginControler( AbstractControler ):
+	def __init__( self, controler_type : str ):
+		super().__init__(controler_type)
+
+
+	def run_session( self ):
+		print("ahah sosi")
+
+
+class RegisterControler( AbstractControler ):
+
+	def __init__( self, controler_type : str ):
+		super().__init__(controler_type)
+
+	def run_session( self ):
+		pass
+
+
+class ReplenishTheBalanceControler( AbstractControler ):
+
+	def __init__( self, controler_type : str ):
+		super().__init__(controler_type)
+
+	def run_session( self ):
+		pass
+
+
+class ViewListOfAllRoomsControler( AbstractControler ):
+
+	def __init__( self, controler_type : str ):
+		super().__init__(controler_type)
+
+	def run_session( self ):
+		pass
+
+
+class BookRoomForDateArrivalControler( AbstractControler ):
+
+	def __init__( self, controler_type : str ):
+		super().__init__(controler_type)
+
+	def run_session( self ):
+		pass
+
+
+
+class BookRoomForDateDepartureControler( AbstractControler ):
+
+	def __init__( self, controler_type : str ):
+		super().__init__(controler_type)
+
+	def run_session( self ):
+		pass
+
+
+
+
 
 class Controler:
 	"""docstring for Controler"""
 	def __init__( self, TypedControler : AbstractControler.__class__ ):
 		self.ControlerTypeClass = TypedControler
 
-		self.ControlerTypeClass.run_session()
-
-
-	def get_login_form_locals_controlers( self ):
-		return self.ControlerTypeClass.Logined
 
 
 	def redirect_to_local_session(self):
-		pass
+		self.ControlerTypeClass.run_session()
